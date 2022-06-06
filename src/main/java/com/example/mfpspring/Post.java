e025b517989c78c3b14c874ec9b1b130aba47ee7
ssyakovlev <win311@gmail.com>
2022-06-06 03:26:34 +0300

    task 1

---
+++ src/main/java/com/example/mfpspring/Post.java
@@ -0,0 +1,20 @@
+package com.example.mfpspring;
+
+//import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String theme;
+    private String content;
+}
